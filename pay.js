2474f1a58aa45d21bd564f93a7970818a25cf7f0
anuprita8540 <borudeanuprita@gmail.com>
2023-08-20 12:49:12 +0530

    payment page are created

---
+++ pay.js
@@ -0,0 +1,6 @@
+function click()
+        {
+           alert("Your payment Is Successfull ");
+           alert("If You Have Join Nritya Academy Click OK Button And fill Given Form, Then You Can Joining Nritya Academy.  ");
+        }
+        click()
