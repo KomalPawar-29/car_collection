c8abfc09284bd2177e2e56c118b40eac636f9672
anuprita8540 <borudeanuprita@gmail.com>
2023-08-20 13:08:39 +0530

    links are added

--- pay.js
+++ pay.js
@@ -1,6 +1,6 @@
 function click()
         {
-           alert("Your payment Is Successfull ");
-           alert("If You Have Join Nritya Academy Click OK Button And fill Given Form, Then You Can Joining Nritya Academy.  ");
+           alert("You Can Pay Here !!!");
+          
         }
         click()
